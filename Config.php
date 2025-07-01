391e62c8f29c6376d3a6c6bdc25b9a5dcb64ccdb
jr <jr1730@scarletmail.rutgers.edu>
2025-07-01 02:41:13 -0400

    1

--- Config.php
+++ Config.php
@@ -1,3 +1,6 @@
 <?php
-$con = mysqli_connect('localhost','root','','project');
+$con = mysqli_connect('db.be-mons1.bengt.wasmernet.com','37fa97ec76718000b6f343583231','068637fa-97ed-77b4-8000-b0fb804bed81','
+dbEKi3kVjaaSsuHsNPME7oTT');
+
+$app = mysqli_select_db($con, 'project');
 ?>
--- admin/product/Config.php
+++ admin/product/Config.php
@@ -1,6 +1,7 @@
 <?php
 
 
-$con = mysqli_connect('localhost','root', '','project');
+$con = mysqli_connect('db.be-mons1.bengt.wasmernet.com','37fa97ec76718000b6f343583231','068637fa-97ed-77b4-8000-b0fb804bed81','
+dbEKi3kVjaaSsuHsNPME7oTT');
 
 ?>
